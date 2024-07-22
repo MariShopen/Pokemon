2b201e545a59264cef9b024a016c1c1d0c85129d
Marina Shopina <marinashopina95@gmail.com>
2024-07-22 22:16:55 +0100

    feat: previous, next button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { ChangeEvent, useState } from "react";
 import { PokemonCard } from "./components/PokemonCard";
 import { Input } from "@/components/ui/input";
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
 export type PokemonInfo = {
   id: number;
@@ -14,7 +15,7 @@ export type PokemonInfo = {
   types: any[];
 };
 
-export const pokemonFetch = async (searchPok: string) => {
+export const pokemonFetch = async (searchPok: string | number) => {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${searchPok}`
   );
@@ -43,22 +44,64 @@ export function App() {
     }
   };
 
+  const handlePokemonPrevious = async () => {
+    let previousPokemonId: number;
+    previousPokemonId = pokemon.id - 1;
+    if (pokemon.id === 1) {
+      previousPokemonId = 1025;
+    }
+    try {
+      let pokemonInfo: PokemonInfo = await pokemonFetch(previousPokemonId);
+      setPokemon(pokemonInfo);
+      setError(null);
+    } catch {
+      setError("something went wrong, please try again");
+      setPokemon(undefined);
+    }
+  };
+
+  const handlePokemonNext = async () => {
+    let nextPokemonId: number;
+    nextPokemonId = pokemon.id + 1;
+    if (pokemon.id === 1025) {
+      nextPokemonId = 1;
+    }
+    try {
+      let pokemonInfo: PokemonInfo = await pokemonFetch(nextPokemonId);
+      setPokemon(pokemonInfo);
+      setError(null);
+    } catch {
+      setError("something went wrong, please try again");
+      setPokemon(undefined);
+    }
+  };
+
   return (
     <div className="flex min-w-full min-h-full h-screen items-center justify-center flex-col bg-slate-700">
       <div className="flex w-[720px] h-[480px] bg-slate-600 flex-col rounded-xl p-5">
-        <div className="flex flex-row justify-center my-5">
+        <div className="flex flex-row justify-center mb-5">
           <Input
             placeholder="Pokemon name"
             onChange={changeHandler}
-            className="mr-4 my-4"
+            className="mr-4"
           ></Input>
-          <Button onClick={handlePokemonSearch} className="my-4">
-            Search
-          </Button>
+          <Button onClick={handlePokemonSearch}>Search</Button>
         </div>
         {Error ? <div className="text-slate-400">{error}</div> : <div></div>}
         {pokemon ? (
-          <PokemonCard pokemonInfo={pokemon} />
+          <div className="flex flex-row justify-between">
+            <Button
+              variant="default"
+              size="icon"
+              onClick={handlePokemonPrevious}
+            >
+              <ChevronLeftIcon className="h-4 w-4" />
+            </Button>
+            <PokemonCard pokemonInfo={pokemon} />
+            <Button variant="default" size="icon" onClick={handlePokemonNext}>
+              <ChevronRightIcon className="h-4 w-4" />
+            </Button>
+          </div>
         ) : (
           <div className="h-3/4"></div>
         )}
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,5 @@
 import { PokemonInfo } from "../App";
 import { Button } from "@/components/ui/button";
-import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 export type PokemonCardProps = {
   pokemonInfo?: PokemonInfo;
   onClickNext?: () => unknown;
@@ -11,25 +10,13 @@ export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemonInfo }) => {
   let weightKg = pokemonInfo?.weight / 10;
   let heightM = pokemonInfo?.height / 10;
   return (
-    <div className="text-slate-400 flex justify-between mb-5">
-      <div className="previous flex">
-        <Button variant="default" size="icon">
-          <ChevronLeftIcon className="h-4 w-4" />
-        </Button>
-      </div>
-      <div className="pokemon_info flex flex-col items-center">
-        <div>#{pokemonInfo?.id}</div>
-        <div>{pokemonInfo?.name}</div>
-        <div>Weight {weightKg} kg</div>
-        <div>Height {heightM} m</div>
-        <div>
-          <img className="h-[250px] w-[250px]" src={avatar} alt="" />
-        </div>
-      </div>
-      <div className="next flex">
-        <Button variant="default" size="icon">
-          <ChevronRightIcon className="h-4 w-4" />
-        </Button>
+    <div className="pokemon_info flex flex-col items-center text-slate-400  mb-5">
+      <div>#{pokemonInfo?.id}</div>
+      <div>{pokemonInfo?.name}</div>
+      <div>Weight {weightKg} kg</div>
+      <div>Height {heightM} m</div>
+      <div>
+        <img className="h-[250px] w-[250px]" src={avatar} alt="" />
       </div>
     </div>
   );
